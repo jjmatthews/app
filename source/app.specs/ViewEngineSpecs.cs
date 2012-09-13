bdfa6e52f7789cb033fca5580089423343230a0c
Josh <josh@jjmatthews.co.uk>
2012-09-13 13:03:41 +0100

    Pushing new changes

---
+++ source/app.specs/ViewEngineSpecs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using app.web.core;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs
+{
+    [Subject(typeof(ViewEngine))]
+    public class ViewEngineSpecs
+    {
+        public abstract class concern : Observes<IImplementAFeature,ViewEngine>
+        {
+            Establish context = () => { };
+        }
+    }
+
+    public class ViewEngine : IImplementAFeature
+    {
+    }
+}
