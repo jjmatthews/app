ccc2ffdec43190a7aae60bc5de2e52d82de3d41e
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-14 08:34:36 -0600

    Updated

---
+++ source/app.specs/ExpressionSpecs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq.Expressions;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{
+  public class ExpressionSpecs
+  {
+    public abstract class concern : Observes
+    {
+    }
+
+    public class when_getting_the_name_of_an_property_from_an_expression_tree : concern
+    {
+      It should_get_the_name = () =>
+      {
+        var jp = new Person {first_name = "JP"};
+        Expression<Func<Person, bool>> condition = x => x.first_name.StartsWith("blah") && x.age < 10 && x.age > 1;
+        ((MemberExpression) condition.Body).Member.Name.ShouldEqual("first_name");
+      };
+    }
+
+    public class when_building_up_code_using_an_expression_tree : concern
+    {
+      It should_be_able_To_builda__Method = () =>
+      {
+        Func<int, bool> is_even = x => x%2 == 0;
+
+        Expression.Lambda<Func<int,bool>>()
+      };
+    }
+    public class Person
+    {
+      public int age { get; set; };
+      public string first_name { get; set; }
+    }
+  }
+}
--- source/app.specs/utility/PersonSpecs.cs
+++ source/app.specs/utility/PersonSpecs.cs
@@ -1,56 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Machine.Specifications;
-using app.utility;
 using developwithpassion.specifications.rhinomocks;
 
 namespace app.specs.utility
 {
-    public class PersonSpecs
+  public class PersonSpecs
+  {
+    public abstract class concern : Observes
     {
-        public abstract class concern : Observes
-        {
-            Establish context = () => { };
-        }
-
-        [Subject(typeof (Person))]
-        public class when_validate_person_model : concern
-        {
-            Establish context = () =>
-                                    {
-
-                                        Validate.Where<Person>.FirstName.shoudlnotbeempty.and.LastName.
-                                            shouldnotbeemptyand.FirstName.shouldnotequal.LastName.and.birthdate.
-                                            shouldbegreaterthanorequalto(mindate).and.birthdate.
-                                            shouldbelessthanorequalto(maxdate);
-
-                                    };
-
-            Because b = () =>
-                            {
-                                
-                            };
-
-            It should_validate = () =>
-                                     {
-                                         
-                                     };
-
-            static Person person_model;
-            static bool result;
-            static string person_rule;
-        }
+      Establish context = () =>
+      {
+      };
     }
 
-    public class Person
+    [Subject(typeof(Person))]
+    public class when_validate_person_model : concern
     {
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public DateTime BirthDate { get; set; }
+      It should_validate = () =>
+      {
+//        var rule_set = RuleBuilder.to_validate<Person>()
+//          .check(x => x.FirstName, c =>
+//          {
+//            c.is_not_null();
+//            c.constraint_error("First name is required on a person");
+//          })
+//          .check(x => x.LastName, c => c.is_not_null())
+//          .check(x => x.FirstName, c => c.is_not_same_as(y => y.LastName))
+//          .check(x => x.BirthDay, c => c.falls_after(new DateTime(1, 1, 1950)));
+      };
 
-
-
+      static Person person_model;
+      static bool result;
+      static string person_rule;
     }
+  }
+
+  public class Person
+  {
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public DateTime BirthDate { get; set; }
+  }
 }
