3820e23f7c68508346bc56a9f70e22db3690936b
Josh <josh@jjmatthews.co.uk>
2012-09-14 10:46:46 +0100

    Pushing new changes

--- source/app.specs/utility/LogSpecs.cs
+++ source/app.specs/utility/LogSpecs.cs
@@ -5,35 +5,39 @@ using developwithpassion.specifications.extensions;
 
 namespace app.specs.utility
 {
-  public class LogSpecs
-  {
-    public abstract class concern : Observes<Log>
+    public class LogSpecs
     {
+        public abstract class concern : Observes<Log>
+        {
+        }
+
+        
+        [Subject(typeof(Log))]
+        public class when_accessing_logging_functionality : concern
+        {
+            Establish context = () =>
+            {
+                logger = fake.an<ILog>();
+                log_factory = fake.an<ICreateLoggers>();
+
+                log_factory.setup(x => x.create_logger_bound_to(typeof(when_accessing_logging_functionality))).Return(logger);
+
+
+
+                GetLoggingFactory_Behaviour item = () => log_factory;
+                spec.change(() => Log.logging_factory_resolution).to(item);
+            };
+
+            Because b = () =>
+              result = Log.an;
+
+            It should_return_a_logger_bound_to_the_calling_type = () =>
+              result.ShouldEqual(logger);
+
+            static ILog logger;
+            static ILog result;
+            static GetLoggingFactory_Behaviour logging_factory_behaviour;
+            static ICreateLoggers log_factory;
+        }
     }
-
-    [Subject(typeof(Log))]
-    public class when_accessing_logging_functionality : concern
-    {
-      Establish context = () =>
-      {
-        logger = fake.an<ILog>();
-        log_factory = fake.an<ICreateLoggers>();
-
-        log_factory.setup(x => x.create_logger_bound_to(typeof(when_accessing_logging_functionality))).Return(logger);
-        GetLoggingFactory_Behaviour item = () => log_factory;
-        spec.change(() => Log.logging_factory_resolution).to(item);
-      };
-
-      Because b = () =>
-        result = Log.an;
-
-      It should_return_a_logger_bound_to_the_calling_type = () => 
-        result.ShouldEqual(logger);
-
-      static ILog logger;
-      static ILog result;
-      static GetLoggingFactory_Behaviour logging_factory_behaviour;
-      static ICreateLoggers log_factory;
-    }
-  }
 }
